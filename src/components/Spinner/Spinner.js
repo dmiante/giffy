1f5afeccc382a916a3b8c95b0ad8e3a2c182d362
dmi4n <the.dmian@live.cl>
2022-08-02 21:04:25 -0400

    Spinner agregado

---
+++ src/components/Spinner/Spinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css'
+
+export default function Spinner () {
+    return (
+        <div class="spinner">
+            <div class="bounce1"></div>
+            <div class="bounce2"></div>
+            <div class="bounce3"></div>
+        </div>
+    )
+}
--- src/pages/SearchResults/SearchResults.js
+++ src/pages/SearchResults/SearchResults.js
@@ -2,21 +2,32 @@ import React, { useEffect, useState } from 'react';
 import ListOfGifs from '../../components/ListOfGifs/ListOfGifs';
 import getGifs from '../../services/getGifs';
 import SearchBar from '../../components/SearchBar/SearchBar'
+import Spinner from '../../components/Spinner/Spinner';
 
 function SearchResults ({ params }){
   
   const {keyword} = params;
   const [gifs, setGifs] = useState([]);
+  const [loading, setLoading] = useState(false);
 
     useEffect(() => {
+      setLoading(true)
       getGifs({ keyword })
-      .then(gifs => 
-        setGifs(gifs))
+      .then(gifs => {
+        setGifs(gifs)
+        setLoading(false)
+      })
       }, [keyword])
 
     return <>
-          <SearchBar />
-          <ListOfGifs gifs={gifs}/>
+          {loading ?
+          <Spinner />
+          :
+          <div>
+            <SearchBar />
+            <ListOfGifs gifs={gifs}/>
+          </div>
+          }
         </>
 }
 
