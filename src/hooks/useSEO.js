064715a4ae800ef38e8053d54aadcec1b33af876
dmi4n <the.dmian@live.cl>
2022-09-27 23:34:43 -0300

    ADD SEO

---
+++ src/hooks/useSEO.js
@@ -0,0 +1,26 @@
+import { useEffect, useRef } from "react";
+
+export default function useSEO({ title, description }) {
+  const prevTitle = useRef(document.title);
+  const prevDescription = useRef(document.querySelector('meta[name="description"]').getAttribute('contents'))
+
+  useEffect(() => {
+    const previousTitle = prevTitle.current;
+    if (title) {
+      document.title = `${title} | Giffy`;
+    }
+
+    return () => (document.title = previousTitle);
+  }, [title]);
+
+  useEffect(() => {
+    const metaDescription = document.querySelector('meta[name="description"]')
+    const previousDescription = prevDescription.current;
+    if (description) {
+      metaDescription.setAttribute('content', description)
+    }
+
+    return () => metaDescription.setAttribute('content', previousDescription)
+
+  }, [description]);
+}
--- src/pages/Detail/Detail.js
+++ src/pages/Detail/Detail.js
@@ -3,13 +3,17 @@ import useSingleGif from '../../hooks/useSingleGif';
 import Gif from '../../components/Gif/Gif'
 import Spinner from '../../components/Spinner/Spinner';
 import { Redirect } from 'wouter';
+import useSEO from '../../hooks/useSEO';
 
 function Detail ({params}){
     const {gif, isLoading, isError} = useSingleGif({id: params.id})
+    const title = gif ? gif.title : ''
+    useSEO({title, description: `Detail of ${title}`})
     
     if(isLoading) return <Spinner />
     if(isError) return <Redirect to='/404'/>
     if (!gif) return null
+
 
     return <>
         <h3 className='App-title'>{gif.title}</h3>
