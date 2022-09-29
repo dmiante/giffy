b3cc5efd1b23ebf1ff47b31bce00f97ee5fe9594
dmi4n <the.dmian@live.cl>
2022-09-29 17:29:54 -0300

    Add SEO class

--- src/pages/Detail/Detail.js
+++ src/pages/Detail/Detail.js
@@ -4,18 +4,30 @@ import Gif from '../../components/Gif/Gif'
 import Spinner from '../../components/Spinner/Spinner';
 import { Redirect } from 'wouter';
 import useSEO from '../../hooks/useSEO';
+import {Helmet} from 'react-helmet'
 
 function Detail ({params}){
     const {gif, isLoading, isError} = useSingleGif({id: params.id})
     const title = gif ? gif.title : ''
     useSEO({title, description: `Detail of ${title}`})
     
-    if(isLoading) return <Spinner />
+    if(isLoading){
+        return (
+            <>
+                <Helmet>
+                    <title>Loading...</title>
+                </Helmet>
+                <Spinner />
+            </>
+    )} 
     if(isError) return <Redirect to='/404'/>
     if (!gif) return null
 
 
     return <>
+        <Helmet>
+            <title>{title} || Giffy</title>
+        </Helmet>
         <h3 className='App-title'>{gif.title}</h3>
         <Gif {...gif}/>
     </>
