554a487e37dfc188ebb0d03eef8163e0c134a31b
damian <the.dmian@live.cl>
2023-03-14 02:19:57 -0300

    UPDATE limit gifs

--- src/services/getGifs.js
+++ src/services/getGifs.js
@@ -15,7 +15,7 @@ const fromApiResponseToGifs = apiResponse => {
         return []
 }
 
-function getGifs({limit = 5, keyword = 'morty', page = 0, rating = 'g'}){
+function getGifs({limit = 10, keyword = 'morty', page = 0, rating = 'g'}){
     const apiURL = `${API_URL}/gifs/search?api_key=${API_KEY}&q=${keyword}&limit=${limit}&offset=${page * limit}&rating=${rating}&lang=en`
 
 
